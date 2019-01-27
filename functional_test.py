46567829891ca0e1510fd3a68aa5bf714a5cb0c6
Jennifer A'Harrah <jeaharrah@gmail.com>
2019-01-27 17:17:12 -0500

    Updated functional_test.py with unittest class and methods

--- functional_test.py
+++ functional_test.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
+import unittest
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
-browser = webdriver.Firefox(firefox_binary=binary, executable_path='virtualenv\\Scripts\\geckodriver.exe')
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+        gecko_driver = 'virtualenv\\Scripts\\geckodriver.exe'
+        self.browser = webdriver.Firefox(firefox_binary=binary, executable_path=gecko_driver)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
